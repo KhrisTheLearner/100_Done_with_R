44263d92b3784261c1ee4479f99634e1a0b5396d
Khristof MUMARAGISHYIKA <88231007+KhrisTheLearner@users.noreply.github.com>
2024-08-10 02:35:44 +0200

    Add files via upload
    Adding the first installment of the "100_Done_with_R" series projects

---
+++ 2._simple_calc.R
@@ -0,0 +1,6 @@
+a <- as.numeric(readline(prompt = "Enter the first number: "))
+b <- as.numeric(readline(prompt = "Enter the second number: "))
+cat("Sum:", a + b, "\n")
+cat("Difference:", a - b, "\n")
+cat("Product:", a * b, "\n")
+cat("Quotient:", a / b, "\n")
---
+++ 3._odd_even_checker.R
@@ -0,0 +1,6 @@
+num <- as.numeric(readline(prompt = "Enter a number: "))
+if (num %% 2 == 0) {
+  cat("Even\n")
+} else {
+  cat("Odd\n")
+}
